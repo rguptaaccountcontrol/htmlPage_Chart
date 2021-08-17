cea6910a5995e64b262caaf917fd3182c1a6a6b7
Rajneesh Gupta <71780869+rguptaaccountcontrol@users.noreply.github.com>
2021-08-17 14:52:30 -0700

    Add files via upload

---
+++ FirstHtml/TechnicalLib/fn_CalcBounds_AMA.js
@@ -0,0 +1,97 @@
+
+async function fn_CalcBounds_AMA(jAryPr,prType)
+{
+    var NAma, iBarsAgo4StopLoss;
+    var dc, vc, cc, PrExp=0.0;
+    var diff=0.0, diffsqr=0.0, avgdiff=0.0, expsd=0.0;
+    var UpBnd=0.0, DnBnd=0.0, ZValue=0.0;
+    var CloNAma=0.0;
+    var rtnJsonAry = [];  // json array to return
+
+    switch(prType) {
+        case 1:  // daily
+            NAma = 20;
+            iBarsAgo4StopLoss = 10;
+        break;
+        case 2:  // weekly
+            NAma = 20;
+            iBarsAgo4StopLoss = 13;
+        break;
+        case 3:  // monthly
+            NAma = 20;
+            iBarsAgo4StopLoss = 13;
+        break;
+        default:
+            NAma = 20;
+            iBarsAgo4StopLoss = 10;
+    }
+
+
+    for(var i=0;i<jAryPr.length;i++)
+    {
+        var dt = jAryPr[i].Date;
+        //var hi = Number(jAryPr[i].High);
+        //var lo = Number(jAryPr[i].Low);
+        var clo = Number(jAryPr[i].Close);
+
+        var obj = {};  // to create the json object
+        //console.log(jAryPr[i]);
+        if (i > NAma)
+        {
+            
+
+            CloNAma = Number(jAryPr[i-NAma].Close); // clo price NAma days ago
+
+            dc = (clo - CloNAma);  // direction value for NAma bars
+            vc=0;
+
+            for(var j=(i-NAma+1);j<=i;j++)
+            {
+                var CloPr = Number(jAryPr[j].Close);
+                var PriorClo = Number(jAryPr[j-1].Close);
+                
+                vc = vc + Math.abs((CloPr - PriorClo));  // volatility value on which smoothing constant is determined
+            }
+
+            cc = 0;
+            if (vc > 0 )
+                cc = (dc/vc)*(dc/vc);   // smoothing constant
+
+            PrExp = PrExp*(1-cc) + (cc*clo);
+
+            ///////////////////////////////////////////////////////////////////////////
+            // now we start calculating bounds
+            diff = clo - PrExp;                         // find the difference in price and price average
+            diffsqr = diff*diff;                        // square the difference to remove the sign
+            avgdiff = avgdiff*0.95 + 0.05*diffsqr;      // average the square of diff
+            expsd = Math.sqrt(avgdiff);                 // get the square root of the avg diff to get the S.D.
+
+            UpBnd = PrExp + expsd*1.64;                 // upper band
+            DnBnd = PrExp - expsd*1.64;                 // lower band
+
+            if (expsd > 0)
+                ZValue = (clo - PrExp)/expsd;
+            else
+                ZValue = 0.0; 
+        }
+        else
+        {
+            // the default values will be here.
+        }
+
+        //console.log(dt,i,clo,PrExp,UpBnd,DnBnd,ZValue,"..",diff,diffsqr,avgdiff,expsd);
+
+        obj["Date"] = dt;
+        //obj["High"] = hi.toFixed(2);
+        //obj["Low"] = lo.toFixed(2);
+        obj["Close"] = clo.toFixed(2);
+        obj["AMA"] = PrExp.toFixed(2);
+        obj["UpBnd"] = UpBnd.toFixed(2);
+        obj["DnBnd"] = DnBnd.toFixed(2);
+        obj["ZValue"] = ZValue.toFixed(2);
+
+        rtnJsonAry.push(obj);
+    }
+
+    return rtnJsonAry;
+}
---
+++ FirstHtml/TechnicalLib/fn_CalcStocastics.js
@@ -0,0 +1,101 @@
+async function fn_CalcStocastics(jAryPr, lookBackDays, iSmoothingKdays, iSmoothingDdays) // jAryPr is the json array with price data
+{
+    var sto = 0.0;
+    var stoAry = [];
+    var rtnJsonAry = [];  // json array to return
+    var hi = 0.0, lo = 0.0, clo = 0.0, hh= -9999.0, ll = 9999.0;
+
+    for(var i=0;i<jAryPr.length;i++)
+    {
+        if (i > lookBackDays)
+        {
+            hh = -9999.0;  // initialize highest high
+            ll = 9999.0;  // initialize lowest low
+            clo = Number(jAryPr[i].Close).toFixed(2);
+
+            for(var j=(i-lookBackDays+1); j<=i; j++)  // find the highest high and the lowest low in the last lookbackdays
+            {
+                hi = Number(jAryPr[j].High).toFixed(2);
+                lo = Number(jAryPr[j].Low).toFixed(2);
+
+                if (hi > hh)    // highest high in lookBackDays
+                    hh = hi;
+
+                if (lo < ll)    // lowest low in lookBackDays
+                    ll = lo;
+            }
+
+            if (hh === 1)
+                sto = 0.0;
+            else
+                sto = ((clo-ll)/(hh-ll)*100).toFixed(2)   // stocastics for lookBackPeriod
+        }
+        else
+        {
+            sto = 0.0;
+        }
+
+        stoAry.push(sto);
+
+        //console.log(jAryPr[i].Date, i, Number(jAryPr[i].High).toFixed(2), Number(jAryPr[i].Low).toFixed(2), clo, hh, ll, sto);
+
+    }
+
+    //console.log(stoAry);
+
+    var StoKAry = SMAforAry(stoAry, iSmoothingKdays); // stocastics K
+    var SroDAry = SMAforAry(StoKAry, iSmoothingDdays); // stocastics D
+    
+    //////////////////////////////////////////////////
+    // create return array
+    var sStoK = "StoK"+lookBackDays+"B";
+    var sStoD = "StoD"+lookBackDays+"B";
+
+    for(var i=0;i<jAryPr.length;i++)
+    {
+        var obj = {};  // to create the json object
+
+        obj["Date"] = jAryPr[i].Date;
+        obj["High"] = Number(jAryPr[i].High);
+        obj["Low"] = Number(jAryPr[i].Low);
+        obj["Close"] = Number(jAryPr[i].Close);
+        obj[sStoK] = Number(StoKAry[i]).toFixed(2);
+        obj[sStoD] = Number(SroDAry[i]).toFixed(2);
+
+        rtnJsonAry.push(obj);
+    }
+
+    return rtnJsonAry;
+
+}
+
+/////////////////////////////////////////////////////
+
+function SMAforAry(aaAry, bars)
+{
+    var tot = 0, OutAvg = 0;
+    var OutAvgAry = [];
+
+    for(var i=0;i<aaAry.length;i++)
+    {
+        var v = Number(aaAry[i]);
+
+        if (i < bars)
+        {
+            tot = tot + v;
+            OutAvg = v;  // we pass the same value back as average
+        }
+        else
+        {
+            tot = tot + v - Number(aaAry[i-bars]);
+            OutAvg = tot/bars;
+            
+            //console.log("else",i,dt,OutAvg,tot);
+        }
+
+        OutAvgAry.push(OutAvg);
+    }
+
+    return OutAvgAry;
+
+}
